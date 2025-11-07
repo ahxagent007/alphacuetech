e247cbb9468aa37470e3af10d7fd2b8d17e5d45f
AHXagent007 <ahx.agent007@gmail.com>
2025-11-07 16:43:56 +0600

    media configured

--- alphacue/settings.py
+++ alphacue/settings.py
@@ -136,10 +136,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
 
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = 'STATIC_ROOT'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
